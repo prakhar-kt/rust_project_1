b1876784e6a8552149ab1e2227edfdb9ea18e6a9
prakhar-kt <79835002+prakhar-kt@users.noreply.github.com>
2023-03-29 21:55:48 +0530

    created a new cargo project

---
+++ mars_cal/target/debug/deps/mars_cal-59087c11ca3f6059.d
@@ -0,0 +1,5 @@
+/Users/hobbes/Documents/rust_projects/mars_cal/target/debug/deps/mars_cal-59087c11ca3f6059: src/main.rs
+
+/Users/hobbes/Documents/rust_projects/mars_cal/target/debug/deps/mars_cal-59087c11ca3f6059.d: src/main.rs
+
+src/main.rs:
---
+++ mars_cal/target/debug/mars_cal.d
@@ -0,0 +1,1 @@
+/Users/hobbes/Documents/rust_projects/mars_cal/target/debug/mars_cal: /Users/hobbes/Documents/rust_projects/mars_cal/src/main.rs
